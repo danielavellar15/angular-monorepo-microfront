0fb70da38ec1805cf08d0cbb90ab54bdcc8266d2
danielavellar15 <danielavellar15@hotmail.com>
2024-06-24 15:34:26 -0300

    header no base project

--- apps/shell/src/app/app.component.ts
+++ apps/shell/src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { HeaderComponent } from '@angular-monorepo/ui';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
